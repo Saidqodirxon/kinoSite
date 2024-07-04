f4497db88e9b9e91abeda0ccf4466b37a400b26b
Saidqodirxon Rahimov <SaidqodirxonUz@gmail.com>
2024-07-05 00:39:10 +0500

    devtools ochish bloklandi

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import MoviePage from "./pages/Movies/Movies";
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
+  React.useEffect(() => {
+    const handleKeyDown = (e) => {
+      // F12 va Ctrl+Shift+I, Ctrl+Shift+J kabi kombinatsiyalarni bloklash
+      if (
+        e.key === "F12" ||
+        (e.ctrlKey && e.shiftKey && (e.key === "I" || e.key === "J"))
+      ) {
+        e.preventDefault();
+        e.stopPropagation();
+      }
+    };
+
+    const handleContextMenu = (e) => {
+      // O'ng tugmani bosish orqali devtoolsni ochish mumkinligini bloklash
+      e.preventDefault();
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener("contextmenu", handleContextMenu);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("contextmenu", handleContextMenu);
+    };
+  }, []);
+
   return (
     <Router>
       <div className="container-big">
